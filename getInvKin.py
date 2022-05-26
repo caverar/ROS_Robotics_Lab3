Rotation fixed

getInvKin.py
@@ -16,57 +16,60 @@ def getInvKin(T, l):
         [q1d q2d q3d q4d]
         [q1u q2u q3u q4u]
     """
-    q = np.double([[0, 0, 0, 0], [0, 0, 0, 0]])
+    try:
+        q = np.double([[0, 0, 0, 0], [0, 0, 0, 0]])
 
-    #%Cálculo de la primera articulación en radianes
-    q[0,0] = math.atan2(T[1,3], T[0,3]) #Codo abajo
-    q[1,0] = q[0,0] #Codo arriba
+        #%Cálculo de la primera articulación en radianes
+        q[0,0] = math.atan2(T[1,3], T[0,3]) #Codo abajo
+        q[1,0] = q[0,0] #Codo arriba
 
+        
+        #Desacople de muñeca:
+        #Cálculo de la posición de la muñeca W
+        Pos_w = T[0:3, 3] - l[3]*T[0:3, 2]
+        #print("Pos_w: ", Pos_w)
+        #Solución de mecanismo 2R para q2 y q3
+        h = round(Pos_w[2] - l[0],3)
+        r = round(math.sqrt(Pos_w[0]**2 + Pos_w[1]**2),3)
+        #print("h: ", h)
+        #print("r: ", r)
+        #Codo abajo:
+        
+        #Tercera articulación en radianes   
+        
+        q[0,2] = math.acos(round((r**2+h**2-l[1]**2-l[2]**2)/(2*l[1]*l[2]), 2))
+        #Segunda articulación en radianes sin offset
+        q[0,1] = math.atan2(h,r) - math.atan2(l[2]*math.sin(q[0,2]), l[1]+l[2]*math.cos(q[0,2]))
+        #Teniendo en cuenta offset en la segunda articulación
+        q[0,1] = q[0,1] - math.pi/2
+
+        #Codo arriba: 
+
+        #Tercera articulación en radianes: negativo de codo abajo para q3
+        q[1,2]= -q[0,2]
+        #Segunda articulación en radianes sin offset
+        q[1,1] = math.atan2(h,r) + math.atan2(l[2]*math.sin(q[0,2]), l[1]+l[2]*math.cos(q[0,2]))
+        #Teniendo en cuenta offset en la segunda articulación
+        q[1,1] = q[1,1] - math.pi/2;   
+
+        #Obtención de valor de cuarta articulación usando vector |a| de T
+        phi = math.atan2(T[2,2], math.sqrt(T[0,2]**2 +T[1,2]**2)) - math.pi/2
+        q[0,3] = phi - q[0,1] -q[0,2]
+        q[1,3] = phi - q[1,1] -q[1,2]
+
+        # print("Codo abajo: ")
+        # print("1: ", q[0,0])
+        # print("2: ", q[0,1])
+        # print("3: ", q[0,2])
+        # print("4: ", q[0,3])
+
+        # print("Codo arriba: ")
+        # print("1: ", q[1,0])
+        # print("2: ", q[1,1])
+        # print("3: ", q[1,2])
+        # print("4: ", q[1,3])
+        return q
+        
+    except ValueError:
+        print("That position can't be reached with this configuration. Try another movement or configuration")
     
-    #Desacople de muñeca:
-    #Cálculo de la posición de la muñeca W
-    Pos_w = T[0:3, 3] - l[3]*T[0:3, 2]
-    print("Pos_w: ", Pos_w)
-    #Solución de mecanismo 2R para q2 y q3
-    h = round(Pos_w[2] - l[0],3)
-    r = round(math.sqrt(Pos_w[0]**2 + Pos_w[1]**2),3)
-    print("h: ", h)
-    print("r: ", r)
-    #Codo abajo:
-    
-    #Tercera articulación en radianes   
-    
-    q[0,2] = math.acos(round((r**2+h**2-l[1]**2-l[2]**2)/(2*l[1]*l[2]), 2))
-    #Segunda articulación en radianes sin offset
-    q[0,1] = math.atan2(h,r) - math.atan2(l[2]*math.sin(q[0,2]), l[1]+l[2]*math.cos(q[0,2]))
-    #Teniendo en cuenta offset en la segunda articulación
-    q[0,1] = q[0,1] - math.pi/2
-
-    #Codo arriba: 
-
-    #Tercera articulación en radianes: negativo de codo abajo para q3
-    q[1,2]= -q[0,2]
-    #Segunda articulación en radianes sin offset
-    q[1,1] = math.atan2(h,r) + math.atan2(l[2]*math.sin(q[0,2]), l[1]+l[2]*math.cos(q[0,2]))
-    #Teniendo en cuenta offset en la segunda articulación
-    q[1,1] = q[1,1] - math.pi/2;   
-
-    #Obtención de valor de cuarta articulación usando vector |a| de T
-    phi = math.atan2(T[2,2], math.sqrt(T[0,2]**2 +T[1,2]**2)) - math.pi/2
-    q[0,3] = phi - q[0,1] -q[0,2]
-    q[1,3] = phi - q[1,1] -q[1,2]
-
-    # print("Codo abajo: ")
-    # print("1: ", q[0,0])
-    # print("2: ", q[0,1])
-    # print("3: ", q[0,2])
-    # print("4: ", q[0,3])
-
-    # print("Codo arriba: ")
-    # print("1: ", q[1,0])
-    # print("2: ", q[1,1])
-    # print("3: ", q[1,2])
-    # print("4: ", q[1,3])
-
-    return q
-

invKin.py
@@ -63,29 +63,41 @@ class Movement:
          self.step = step
 
 def moveRobot(T, l, movement, direction):
-    codo = 0 #0: down, 1: up
-    print("Before: ", T)
-    print("Previous position: ", T[:,3])
-    changeMatrix(T, movement, direction)
-    print("After: ", T)
-    print("Current position: ", T[:,3])
-    q = np.degrees(getInvKin(T, l))
-    goal_position_raw = deg2raw(q[codo,:])
-    moveJoints(goal_position_raw, q[codo, :])
+    codo = 0 #0: down, 1: up    
+    previous_T = T.copy()
+    #print("Previous position: ", T[:,3])
+    T = changeMatrix(T, movement, direction)
+    #print("Current position: ", T[:,3])
+    try:
+        q = np.degrees(getInvKin(T, l))
+        goal_position_raw = deg2raw(q[codo,:])
+        moveJoints(goal_position_raw, q[codo, :])
+    except:        
+        print("Exception...\n")
+        T = previous_T.copy()
+    return T
+
+
+
 
 def changeMatrix(T, movement, direction):
     if movement.name == "rot":
+        print("Rotation")
         angle = math.radians(movement.step)*direction
         c = math.cos(angle)
         s = math.sin(angle)
-        rot_y = np.array[[c, 0, s], [0, 1, 0], [-s, 0, c]]
-        T[0:3, 0:3] == T[0:3, 0:3]*rot_y
+        rot_y = np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]])
+        T[0:3, 0:3] = np.dot(T[0:3, 0:3],rot_y)
+        
     else:
+        print("Traslation")
         if(movement.name == "trax"): row = 0
         elif(movement.name == "tray"): row = 1
         elif(movement.name == "traz"): row = 2
 
         T[row, 3] = T[row, 3] + movement.step * direction  
+    return T
+
 
 
 def moveJoints(goal_position_raw, q):
@@ -95,6 +107,7 @@ def moveJoints(goal_position_raw, q):
         print("Moving ID:", motors_ids[i], " Angle: ", q[i], " Raw: ", goal_position_raw[i])
 
 def main(step: list = [2, 2 ,2, 45]):
+    
     l = [14.5, 10.7, 10.7, 9]
     
 
@@ -107,6 +120,7 @@ def main(step: list = [2, 2 ,2, 45]):
     TRAY = Movement("tray", step[1])
     TRAZ = Movement("traz", step[2])
     ROT  = Movement("rot",  step[3])    
+    movements = [TRAX, TRAY, TRAZ, ROT]
 
     print("--- Inverse Kinematics with Python and ROS ---")
     # For initial position
@@ -114,34 +128,28 @@ def main(step: list = [2, 2 ,2, 45]):
     q = np.degrees(getInvKin(T, l))
     goal_position_raw = deg2raw(q[0,:])
     moveJoints(goal_position_raw, q[0, :])
-
-
-    moveRobot(T, l, TRAY, -1)
-
-
-    # movements = [TRAX, TRAY, TRAZ, ROT]
-    # i = 3
     
-    # changeMatrix(T, movements[i], -1)
-    # print("After:\n", T)
-
-    # while(True):
-    #     print("Enter key: ")
-    #     key = getKey()
+    i = 0
+    
+    while(True):
+        print("T now:\n", T)
+        print("Enter key: ")
+        key = getKey()
         
-    #     if(key == "w"):
-    #         i = (i+1)%4s
-    #         print("Current movement is: ", movements[i].name, "\n")
+        if(key == "w"):
+            i = (i+1)%4
+            print("Current movement is: ", movements[i].name, "\n")
 
-    #     elif(key == "s"):
-    #         i = (i-1)%4
-    #         print("Current movement is: ", movements[i].name, "\n")
+        elif(key == "s"):
+            i = (i-1)%4
+            print("Current movement is: ", movements[i].name, "\n")
 
-    #     elif(key == "d"):
-    #         print("Movement: ", movements[i].name, " to: ", movements[i].step)
-    #         #moveRobot
-    #     elif(key == "a"):
-    #         print("Movement: ", movements[i].name, " to: ", -1*movements[i].step)
+        elif(key == "d"):
+            print("Movement: ", movements[i].name, " of: ", movements[i].step)
+            T = moveRobot(T, l, movements[i], 1)
+        elif(key == "a"):
+            print("Movement: ", movements[i].name, " of: ", -1*movements[i].step)
+            T = moveRobot(T, l, movements[i], -1)
 
 
 